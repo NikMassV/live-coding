8df252e0c6b5aee0a9d252cb44a0be0335a20080
NikMassV <slipknoshka777@gmail.com>
2023-11-13 12:18:45 +0100

    Add Nth Highest Salary

--- src/test/java/sql/Leetcode Solutions.sql
+++ src/test/java/sql/Leetcode Solutions.sql
@@ -1,7 +1,24 @@
 --https://leetcode.com/problems/second-highest-salary/submissions/
+Second Highest Salary
 
 SELECT MAX(salary) AS SecondHighestSalary
 FROM Employee
 WHERE Salary NOT IN (SELECT MAX(Salary) FROM Employee);
+
+------------------------------------------------------------------------------------------------------------------------
+
+--https://leetcode.com/problems/nth-highest-salary/description/
+Nth Highest Salary
+
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  RETURN QUERY (
+    SELECT DISTINCT e.salary
+    FROM Employee e
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET n-1
+  );
+END;
+$$ LANGUAGE plpgsql;
 
 ------------------------------------------------------------------------------------------------------------------------
