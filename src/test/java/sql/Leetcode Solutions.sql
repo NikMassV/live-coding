76f17815ca26c3f3afd98e259dec350a3accc6ca
NikMassV <slipknoshka777@gmail.com>
2023-12-04 15:54:01 +0100

    Add Rising Temperature

--- src/test/java/sql/Leetcode Solutions.sql
+++ src/test/java/sql/Leetcode Solutions.sql
@@ -191,3 +191,12 @@ DELETE FROM person p1 USING person p2
 WHERE p1.email = p2.email AND p1.id > p2.id;
 
 ------------------------------------------------------------------------------------------------------------------------
+--https://leetcode.com/problems/rising-temperature/description/
+Rising Temperature
+
+SELECT
+  w1.id
+FROM weather w1
+JOIN weather w2 ON w1.recordDate-w2.recordDate=1 AND w1.temperature >w2.temperature;
+
+------------------------------------------------------------------------------------------------------------------------
