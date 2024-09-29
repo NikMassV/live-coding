9f8d0c1df7dbe68570f64e4746cbb8935731c044
slipk <slipknoshka777@gmail.com>
2024-09-30 00:36:30 +0300

    Add Capital Gain/Loss

--- src/test/java/sql/Leetcode Solutions.sql
+++ src/test/java/sql/Leetcode Solutions.sql
@@ -1,4 +1,14 @@
 ------------------------------------------------------------------------------------------------------------------------
+--https://leetcode.com/problems/capital-gainloss/description/
+Capital Gain/Loss
+
+SELECT
+    stock_name,
+    SUM(CASE WHEN operation = 'Sell' THEN price ELSE 0 END) - SUM(CASE WHEN operation = 'Buy' THEN price ELSE 0 END) AS capital_gain_loss
+FROM Stocks
+GROUP BY 1
+
+------------------------------------------------------------------------------------------------------------------------
 --https://leetcode.com/problems/restaurant-growth/description/
 Restaurant Growth
 
