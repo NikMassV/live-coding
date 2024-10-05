dd6eb4bde50780ca8bc2e850768dbe4bdde055e1
slipk <slipknoshka777@gmail.com>
2024-10-05 23:46:38 +0300

    Add Three Integer Sum solution through Two pointers

--- src/test/java/ThreeIntegerSum.java
+++ src/test/java/ThreeIntegerSum.java
@@ -9,9 +9,36 @@ public class ThreeIntegerSum {
     @Test
     public void test() {
         assertEquals(List.of(List.of(-1, -1, 2), List.of(-1, 0, 1)), threeSumMap(new int[]{-1, 0, 1, 2, -1, -4}));
+        assertEquals(List.of(List.of(-1, -1, 2), List.of(-1, 0, 1)), threeSumTwoPointers(new int[]{-1, 0, 1, 2, -1, -4}));
     }
 
-    public List<List<Integer>> threeSumMap(int[] nums) {
+    private List<List<Integer>> threeSumTwoPointers(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum > 0) {
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private List<List<Integer>> threeSumMap(int[] nums) {
         Arrays.sort(nums);
         Map<Integer, Integer> count = new HashMap<>();
         for (int num : nums) {
