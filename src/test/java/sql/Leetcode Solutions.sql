9101b27be44908cc7cdc6b59a5bb498d47adb84c
slipk <slipknoshka777@gmail.com>
2024-10-15 22:22:41 +0300

    Add Department Top Three Salaries

--- src/test/java/sql/Leetcode Solutions.sql
+++ src/test/java/sql/Leetcode Solutions.sql
@@ -1,4 +1,24 @@
 ------------------------------------------------------------------------------------------------------------------------
+--https://leetcode.com/problems/department-top-three-salaries/
+Department Top Three Salaries
+
+SELECT
+    Department,
+    Employee,
+    Salary
+FROM
+    (
+    SELECT
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
+        dense_rank() OVER(PARTITION BY e.departmentId ORDER BY e.salary DESC) AS shorted
+    FROM Department d
+    JOIN Employee e ON d.id = e.departmentId
+    )
+WHERE shorted <= 3
+
+------------------------------------------------------------------------------------------------------------------------
 --https://leetcode.com/problems/odd-and-even-transactions/description/
 Odd and Even Transactions
 
