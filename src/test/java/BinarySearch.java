0f7fef105342a78441efc4bef1354f9a4e1cf912
slipk <slipknoshka777@gmail.com>
2024-10-16 23:24:54 +0300

    Add Binary Search

---
+++ src/test/java/BinarySearch.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BinarySearch {
+
+    @Test
+    public void test() {
+        assertEquals(3, search(new int[]{-1, 0, 2, 4, 6, 8}, 4));
+        assertEquals(-1, search(new int[]{-1, 0, 2, 4, 6, 8}, 3));
+    }
+
+    private int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int m = l + ((r - l) / 2);
+            if (nums[m] > target) {
+                r = m - 1;
+            } else if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                return m;
+            }
+        }
+        return -1;
+    }
+}
