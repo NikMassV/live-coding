d59bdb56eece5e6210d0341c006b40bf81fe7de3
slipk <slipknoshka777@gmail.com>
2024-10-20 22:58:05 +0300

    Add Longest Consecutive Sequence

---
+++ src/test/java/LongestConsecutiveSequence.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LongestConsecutiveSequence {
+
+    @Test
+    public void test() {
+        assertEquals(4, longestConsecutive(new int[]{2, 20, 4, 10, 3, 4, 5}));
+        assertEquals(7, longestConsecutive(new int[]{0, 3, 2, 5, 4, 6, 1, 1}));
+
+    }
+
+    private int longestConsecutive(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+        int longest = 0;
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) {
+                int length = 1;
+                while (numSet.contains(num + length)) {
+                    length++;
+                }
+                longest = Math.max(longest, length);
+            }
+        }
+        return longest;
+    }
+}
