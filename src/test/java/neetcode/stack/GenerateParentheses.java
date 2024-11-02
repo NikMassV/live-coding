239d5a8925d6453c44ab684261c2e2f920de170a
slipk <slipknoshka777@gmail.com>
2024-11-02 22:33:37 +0300

    Add another Generate Parentheses solution

--- src/test/java/neetcode/stack/GenerateParentheses.java
+++ src/test/java/neetcode/stack/GenerateParentheses.java
@@ -13,6 +13,7 @@ public class GenerateParentheses {
     @Test
     public void test() {
         assertEquals(List.of("((()))", "(()())", "(())()", "()(())", "()()()"), generateParenthesis(3));
+        assertEquals(List.of("((()))", "(()())", "(())()", "()(())", "()()()"), generateParenthesis1(3));
     }
 
     private List<String> generateParenthesis(int n) {
@@ -41,5 +42,23 @@ public class GenerateParentheses {
             backtrack(n, openN, closedN + 1, stack, res);
             stack.pop();
         }
+    }
+
+    private List<String> generateParenthesis1(int n) {
+        List<List<String>> res = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            res.add(new ArrayList<>());
+        }
+        res.getFirst().add("");
+        for (int k = 0; k <= n; k++) {
+            for (int i = 0; i < k; i++) {
+                for (String left : res.get(i)) {
+                    for (String right : res.get(k - i - 1)) {
+                        res.get(k).add("(" + left + ")" + right);
+                    }
+                }
+            }
+        }
+        return res.get(n);
     }
 }
