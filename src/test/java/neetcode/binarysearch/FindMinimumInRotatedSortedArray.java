a993fb75c7ad09466ccbc42d1d5b678e8a699095
slipk <slipknoshka777@gmail.com>
2024-11-04 23:21:50 +0300

    Add Find Minimum in Rotated Sorted Array

---
+++ src/test/java/neetcode/binarysearch/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,27 @@
+package neetcode.binarysearch;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FindMinimumInRotatedSortedArray {
+
+    @Test
+    public void test() {
+        assertEquals(0, findMin(new int[]{4, 5, 0, 1, 2, 3}));
+    }
+
+    private int findMin(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r) {
+            int m = l + (l + r) / 2;
+            if (nums[m] < nums[r]) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return nums[l];
+    }
+}
