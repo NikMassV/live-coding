0ec3ac44fe44d77f659c686dee6918b28f4da9f5
slipk <slipknoshka777@gmail.com>
2025-03-12 18:52:54 +0100

    add Customer Who Visited but Did Not Make Any Transactions

--- src/test/java/sql/Leetcode Solutions.sql
+++ src/test/java/sql/Leetcode Solutions.sql
@@ -1,4 +1,16 @@
 ------------------------------------------------------------------------------------------------------------------------
+--https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/
+Customer Who Visited but Did Not Make Any Transactions
+
+SELECT
+    v.customer_id,
+    COUNT(v.customer_id) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t ON v.visit_id = t.visit_id
+WHERE t.visit_id IS NULL
+GROUP BY v.customer_id
+
+------------------------------------------------------------------------------------------------------------------------
 --https://leetcode.com/problems/patients-with-a-condition/description/
 Patients With a Condition
 
