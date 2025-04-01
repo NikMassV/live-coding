21b93282b38401830f4c14559b2037b5fa44ba35
slipk <slipknoshka777@gmail.com>
2025-04-01 23:52:47 +0200

    add Daily Leads and Partners

--- src/test/java/sql/Leetcode Solutions.sql
+++ src/test/java/sql/Leetcode Solutions.sql
@@ -1,4 +1,16 @@
 ------------------------------------------------------------------------------------------------------------------------
+--https://leetcode.com/problems/daily-leads-and-partners/description/
+Daily Leads and Partners
+
+SELECT
+    date_id,
+    make_name,
+    COUNT(DISTINCT lead_id) AS unique_leads,
+    COUNT(DISTINCT partner_id) AS unique_partners
+FROM DailySales
+GROUP BY date_id, make_name
+
+------------------------------------------------------------------------------------------------------------------------
 --https://leetcode.com/problems/invalid-tweets/
 Invalid Tweets
 
