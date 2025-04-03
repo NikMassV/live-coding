1a5d91ab4e5f22b2fd8ef02665d616ac897a72a2
slipk <slipknoshka777@gmail.com>
2025-04-03 22:55:34 +0200

    add Find Students Who Improved

--- src/test/java/sql/Leetcode Solutions.sql
+++ src/test/java/sql/Leetcode Solutions.sql
@@ -1,4 +1,26 @@
 ------------------------------------------------------------------------------------------------------------------------
+--https://leetcode.com/problems/find-students-who-improved/
+Find Students Who Improved
+
+WITH conditions AS(
+    SELECT
+        DISTINCT(student_id),
+        subject,
+        FIRST_VALUE(score) OVER(PARTITION BY student_id, subject ORDER BY exam_date) first_score,
+        LAST_VALUE(score) OVER(PARTITION BY student_id, subject ORDER BY exam_date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) latest_score,
+        COUNT(student_id) OVER(PARTITION BY student_id, subject) diff_dates
+        FROM Scores
+)
+SELECT
+    student_id,
+    subject,
+    first_score,
+    latest_score
+FROM conditions
+WHERE first_score < latest_score AND diff_dates > 1
+ORDER BY student_id, subject
+
+------------------------------------------------------------------------------------------------------------------------
 --https://leetcode.com/problems/daily-leads-and-partners/description/
 Daily Leads and Partners
 
