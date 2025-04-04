9b90624d94e02f3f8d1cde003a22f52cf5342f5a
slipk <slipknoshka777@gmail.com>
2025-04-04 22:48:09 +0200

    add Find Followers Count

--- src/test/java/sql/Leetcode Solutions.sql
+++ src/test/java/sql/Leetcode Solutions.sql
@@ -1,4 +1,15 @@
 ------------------------------------------------------------------------------------------------------------------------
+--https://leetcode.com/problems/find-followers-count/description/
+Find Followers Count
+
+SELECT
+    user_id,
+    COUNT(follower_id) AS followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id
+
+------------------------------------------------------------------------------------------------------------------------
 --https://leetcode.com/problems/find-students-who-improved/
 Find Students Who Improved
 
