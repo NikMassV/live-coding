bce31bb69e352246a7c7932192a9b37579f4cd1a
slipk <slipknoshka777@gmail.com>
2025-04-11 23:36:56 +0200

    add Primary Department for Each Employee

--- src/test/java/sql/Leetcode Solutions.sql
+++ src/test/java/sql/Leetcode Solutions.sql
@@ -928,3 +928,19 @@ FROM Products
 WHERE low_fats = 'Y' and recyclable = 'Y'
 
 ------------------------------------------------------------------------------------------------------------------------
+--https://leetcode.com/problems/primary-department-for-each-employee/
+Primary Department for Each Employee
+
+SELECT
+    employee_id,
+    department_id
+FROM Employee
+WHERE primary_flag = 'Y' OR employee_id IN (
+    SELECT
+        employee_id
+    FROM Employee
+    GROUP BY employee_id
+    HAVING COUNT(employee_id) = 1
+    )
+
+------------------------------------------------------------------------------------------------------------------------
