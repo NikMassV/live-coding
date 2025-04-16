08550a67bfdbace427731db8f22d82c13a634ae5
slipk <slipknoshka777@gmail.com>
2025-04-16 23:36:18 +0200

    add Calculate Special Bonus

--- src/test/java/sql/Leetcode Solutions.sql
+++ src/test/java/sql/Leetcode Solutions.sql
@@ -969,3 +969,13 @@ WHERE primary_flag = 'Y' OR employee_id IN (
     )
 
 ------------------------------------------------------------------------------------------------------------------------
+--https://leetcode.com/problems/calculate-special-bonus/
+Calculate Special Bonus
+
+SELECT
+    employee_id,
+    CASE WHEN employee_id % 2 = 1 AND substring(name FROM 1 FOR 1) != 'M' THEN salary ELSE 0 END AS bonus
+FROM Employees
+ORDER BY employee_id
+
+------------------------------------------------------------------------------------------------------------------------
