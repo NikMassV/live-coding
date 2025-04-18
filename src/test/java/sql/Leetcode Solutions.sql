64096696f880f20f216674d86111b7dcf84df815
slipk <slipknoshka777@gmail.com>
2025-04-18 23:09:41 +0200

    add Employees With Missing Information

--- src/test/java/sql/Leetcode Solutions.sql
+++ src/test/java/sql/Leetcode Solutions.sql
@@ -990,3 +990,14 @@ WHERE EXTRACT(YEAR FROM time_stamp) = 2020
 GROUP BY user_id
 
 ------------------------------------------------------------------------------------------------------------------------
+--https://leetcode.com/problems/employees-with-missing-information/description/
+Employees With Missing Information
+
+SELECT
+    COALESCE(e.employee_id, s.employee_id) AS employee_id
+FROM Employees e
+FULL JOIN Salaries s ON e.employee_id = s.employee_id
+WHERE e.name IS NULL OR s.salary IS NULL
+ORDER BY 1
+
+------------------------------------------------------------------------------------------------------------------------
