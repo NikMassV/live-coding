b4572a7e5d8fa11afdb23e311d659d3a412f74aa
slipk <slipknoshka777@gmail.com>
2025-04-26 23:50:01 +0200

    add Top Travellers

--- src/test/java/sql/Leetcode Solutions.sql
+++ src/test/java/sql/Leetcode Solutions.sql
@@ -1,4 +1,16 @@
 ------------------------------------------------------------------------------------------------------------------------
+--https://leetcode.com/problems/top-travellers/description/
+Top Travellers
+
+SELECT
+    u.name,
+    COALESCE(SUM(distance), 0) AS travelled_distance
+FROM Users u
+LEFT JOIN Rides r ON u.id = r.user_id
+GROUP BY u.id, u.name
+ORDER BY travelled_distance DESC, u.name
+
+------------------------------------------------------------------------------------------------------------------------
 --https://leetcode.com/problems/find-product-recommendation-pairs/description/
 Find Product Recommendation Pairs
 
